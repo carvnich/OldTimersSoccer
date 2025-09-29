5677119242d56db1f794fe47fe8595bdbbf86544
Nicholas Carvalho <nicholasjacarvalho@gmail.com>
2025-09-29 11:23:13 -0400

    set division A as default

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,25 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
-using OldTimersSoccer.Models;
 using OldTimersSoccer.Services;
-using System.Diagnostics;
 
 namespace OldTimersSoccer.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly ISoccerService _soccerService;
 
-        public HomeController(ILogger<HomeController> logger, ISoccerService soccerService)
+        public HomeController(ISoccerService soccerService)
         {
-            _logger = logger;
             _soccerService = soccerService;
         }
 
-        public async Task<IActionResult> Index(string division = null)
+        public async Task<IActionResult> Index()
         {
             var standings = await _soccerService.GetStandingsAsync();
-            ViewBag.Division = division;
+            ViewBag.Division = standings.FirstOrDefault()?.DivisionId ?? "A DIVISION";
             return View(standings);
         }
 
@@ -35,23 +31,6 @@ namespace OldTimersSoccer.Controllers
             var scorers = await _soccerService.GetScorersAsync();
             ViewBag.Division = division;
             return PartialView("_ScorersPartial", scorers);
-        }
-
-        public async Task<IActionResult> Players(string teamId)
-        {
-            var players = await _soccerService.GetGamePlayersAsync(teamId);
-            return View(players);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Services/SoccerService.cs
+++ Services/SoccerService.cs
@@ -6,46 +6,28 @@ namespace OldTimersSoccer.Services
     public class SoccerService : ISoccerService
     {
         private readonly HttpClient _httpClient;
-        private readonly string _baseUrl = "http://oldtimerssoccerleague.com/otsl_api.php";
+        private const string BaseUrl = "http://oldtimerssoccerleague.com/otsl_api.php";
 
-        public SoccerService(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-        }
+        public SoccerService(HttpClient httpClient) => _httpClient = httpClient;
 
-        public async Task<List<Match>> GetMatchesAsync(int year = 2025)
-        {
-            var url = $"{_baseUrl}?otsl_action=get_league_schedule&season_year={year}";
-            return await FetchDataAsync<Match>(url);
-        }
+        public Task<List<Standing>> GetStandingsAsync(int year = 2025) =>
+            FetchDataAsync<Standing>($"{BaseUrl}?otsl_action=show_standings&season_year={year}");
 
-        public async Task<List<Standing>> GetStandingsAsync(int year = 2025)
-        {
-            var url = $"{_baseUrl}?otsl_action=show_standings&season_year={year}";
-            return await FetchDataAsync<Standing>(url);
-        }
+        public Task<List<Scorer>> GetScorersAsync(int year = 2025) =>
+            FetchDataAsync<Scorer>($"{BaseUrl}?otsl_action=get_scorers&season_year={year}");
 
-        public async Task<List<Scorer>> GetScorersAsync(int year = 2025)
-        {
-            var url = $"{_baseUrl}?otsl_action=get_scorers&season_year={year}";
-            return await FetchDataAsync<Scorer>(url);
-        }
+        public Task<List<Match>> GetMatchesAsync(int year = 2025) =>
+            FetchDataAsync<Match>($"{BaseUrl}?otsl_action=get_league_schedule&season_year={year}");
 
-        public async Task<List<Player>> GetGamePlayersAsync(string teamId, int year = 2025, string playerType = "O")
-        {
-            var url = $"{_baseUrl}?otsl_action=get_game_players&team_id={teamId}&season_year={year}&player_type={playerType}";
-            return await FetchDataAsync<Player>(url);
-        }
+        public Task<List<Player>> GetGamePlayersAsync(string teamId, int year = 2025, string playerType = "O") =>
+            FetchDataAsync<Player>($"{BaseUrl}?otsl_action=get_game_players&team_id={teamId}&season_year={year}&player_type={playerType}");
 
         private async Task<List<T>> FetchDataAsync<T>(string url)
         {
             var response = await _httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
-
             var json = await response.Content.ReadAsStringAsync();
-            var data = JsonSerializer.Deserialize<List<T>>(json);
-
-            return data ?? new List<T>();
+            return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
         }
     }
 }
